cc64d6025b727124740c040961ef0ae3596d12e0
Peralta <miguel.peralta@magna.com>
2018-01-18 18:04:37 -0600

    Agregar archivos de proyecto.

---
+++ AtkLecturaCodigoQr2/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using AtkLecturaCodigoQr2.Models;
+
+namespace AtkLecturaCodigoQr2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult Captura()
+        {
+            return View();
+        }
+        public IActionResult Rebind()
+        {
+            string path = "http://localhost:55694/Images/" + "images.png";
+            return Json(path);
+        }
+    }
+}
---
+++ AtkLecturaCodigoQr2/Controllers/PhotoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AtkLecturaCodigoQr2.Controllers
+{
+    public class PhotoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string imageName)
+        {
+            ViewBag.pic = "http://localhost:55694/Images/" + imageName;
+            return View();
+        }
+    }
+}
